04b199bf7eb608f57b9fe002ee1ca79cb56e15c7
Josiah Brown <josiah.brown@acumera.net>
2022-11-09 10:56:24 -0600

    First commit with basic ec2 instance deployment

---
+++ tf_basics/variables.tf
@@ -0,0 +1,10 @@
+
+variable "instance_type" {
+	type = string
+    description = "Stores the EC2 Instance Type"
+}
+
+variable "instance_name" {
+  type = string
+  description = "Stores the EC2 Instance name for later referencing"
+}
